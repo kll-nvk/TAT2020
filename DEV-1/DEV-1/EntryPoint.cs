80db12dffb584307e46fc589250367a9bb763f5b
Kirill Novik <thenavik1337@gmail.com>
2020-03-16 15:14:35 +0300

    added new 1 method and change 1

---
+++ DEV-1/DEV-1/EntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+
+            StringAnalizer stringAnalizer = new StringAnalizer();
+            Console.WriteLine(stringAnalizer.NumberOfRepeatingSymbols(null));
+            Console.WriteLine(stringAnalizer.NumberOfNonRepeatingSymbols(Console.ReadLine()));
+
+        }
+    }
+}
---
+++ DEV-1/DEV-1/StringAnalizer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    class StringAnalizer
+    {
+        public int NumberOfRepeatingSymbols(string word)
+        {
+            int max = 0;
+            int count = 0;
+
+            if (!String.IsNullOrEmpty(word))
+            {
+                char buffer = word[0];
+
+
+                foreach (char c in word)
+                {
+                    if (buffer == c)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if (max < count)
+                        {
+                            max = count;
+                        }
+                        count = 1;
+                        buffer = c;
+                    }
+                }
+
+                if (count > max)
+                {
+                    return count;
+                }
+                else
+                {
+                    return max;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int NumberOfNonRepeatingSymbols(string word)
+        {
+            int max = 0;
+            int count = 0;
+
+            if (!String.IsNullOrEmpty(word))
+            {
+                char buffer = word[0];
+
+                foreach (char c in word)
+                {
+                    if (buffer != c)
+                    {
+                        count++;
+                        buffer = c;
+                    }
+                    else
+                    {
+                        count = 1;
+                        buffer = c;
+                    }
+                }
+
+                if (count > max)
+                {
+                    return count;
+                }
+                else
+                {
+                    return max;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
