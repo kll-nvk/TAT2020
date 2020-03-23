ed674433f4f3cb657385565c630f44ae5c46768b
Kirill Novik <thenavik1337@gmail.com>
2020-03-23 17:21:57 +0300

    add logic and comments in Chassis class

--- DEV-1.3/DEV-1.3/Chassis.cs
+++ DEV-1.3/DEV-1.3/Chassis.cs
@@ -8,6 +8,9 @@ namespace DEV_1._3
         private string _serialNumber;
         private uint _maxLoad;
 
+        /// <summary>
+        ///  Constructor that initialized Chassis class elements 
+        /// </summary>
         public Chassis(byte wheelsNumber, string serialNumber, uint maxLoad)
         {
             WheelsNumber = wheelsNumber;
@@ -15,6 +18,9 @@ namespace DEV_1._3
             _maxLoad = maxLoad;
         }
 
+        /// <summary>
+        /// Method accept and returns WheelsNumber values
+        /// </summary>
         public byte WheelsNumber
         {
             
@@ -34,6 +40,9 @@ namespace DEV_1._3
             }
         }
 
+        /// <summary>
+        /// Method accept and returns Serial number values
+        /// </summary>
         public string SerialNumber
         {
             get
@@ -47,11 +56,30 @@ namespace DEV_1._3
                 {
                     throw new ArgumentException("Please write correct a normal string");
                 }
+                CheckLatinSymbols(value);
                 _serialNumber = value;
             }
         }
 
 
+        /// <summary>
+        /// A method that throws an error if the characters are not Latin alphavit
+        /// </summary>
+        public void CheckLatinSymbols(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (!(Char.IsDigit(value[i]) || value[i] >= 'A' && value[i] <= 'Z'))
+                {
+                    throw new Exception("Please write a symbol which Corresponds to Latin Alfovit");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method that displays information on the console
+        /// </summary>
+        /// <returns>string which contains foreign pharmacy</returns>
         public string GetInfo()
         {
             return $"Chassis:\nWheels Number:{WheelsNumber}\nSerial Number:{SerialNumber}\nMax load:{_maxLoad}\n";
