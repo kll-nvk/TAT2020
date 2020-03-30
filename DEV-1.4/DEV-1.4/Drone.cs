d36b51f8d5c29e32c55836ced59f17f952ccfbfd
Kirill Novik <thenavik1337@gmail.com>
2020-03-30 19:34:53 +0300

    Add logic in Drone class

--- DEV-1.4/DEV-1.4/Drone.cs
+++ DEV-1.4/DEV-1.4/Drone.cs
@@ -1,12 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DEV_1._4
 {
-    class Drone
+    class Drone:IFlyable
     {
-    }
+        private const uint MIN_SPEED = 3;
+        private const uint MAX_SPEED = 120;
+        private const int MAX_DISTANCE = 1000;
+        private const double FLIGHT_TIME = 10f / 60;
+        private const double STOP_TIME = 1f / 60;
+
+        private byte _speed;
+
+        public Drone(Point coordinate, byte speed )
+        {
+            Speed=speed;
+        }
+        public Point Coordinates { get; set; }
+
+        public byte Speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                if (value > MAX_SPEED || value < MIN_SPEED)
+                {
+                    throw new ArgumentException("Incorrect argument value for drone speed");
+                }
+
+                _speed = value;
+            }
+        }
+
+        /// <summary>
+        /// Changes the location of drone
+        /// </summary>
+        /// <param name="newCoordinates">New location</param>
+        public void FlyTo(Point newCoordinates)
+        {
+            if (Coordinates.GetDistance(newCoordinates) > MAX_DISTANCE)
+            {
+                throw new ArgumentException($"This distance is greater than {MAX_DISTANCE}");
+            }
+
+            Coordinates = newCoordinates;
+        }
+
+        /// <summary>
+        /// Counts time for drone to get to argument location
+        /// </summary>
+        /// <param name="newCoordinates">The end location of flight</param>
+        /// <returns>The flight time</returns>
+        public double GetFlyTime(Point newCoordinates)
+        {
+            double distance = Coordinates.GetDistance(newCoordinates);
+
+            if (distance > MAX_DISTANCE)
+            {
+                throw new ArgumentException($"This distance is greater than {MAX_DISTANCE}");
+            }
+
+            double flyTime = distance / _speed;
+
+            return flyTime + flyTime / FLIGHT_TIME * STOP_TIME;
+
+
+        }
 }
--- DEV-1.4/DEV-1.4/EntryPoint.cs
+++ DEV-1.4/DEV-1.4/EntryPoint.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DEV_1._4
 {
@@ -10,6 +6,14 @@ namespace DEV_1._4
     {
         static void Main(string[] args)
         {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
